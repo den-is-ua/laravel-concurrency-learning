0feae4634c7eec5584d4a675a881616562b45ba1
admin <admin@admins-MacBook-Pro.local>
2026-04-01 07:44:55 +0300

    add defer

--- app/Console/Commands/ConcurrencyDeferCommand.php
+++ app/Console/Commands/ConcurrencyDeferCommand.php
@@ -5,8 +5,10 @@ namespace App\Console\Commands;
 use Illuminate\Console\Attributes\Description;
 use Illuminate\Console\Attributes\Signature;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Concurrency;
+use Illuminate\Support\Facades\Log;
 
-#[Signature('app:concurrency-defer-command')]
+#[Signature('concurrency:defer')]
 #[Description('Command description')]
 class ConcurrencyDeferCommand extends Command
 {
@@ -15,6 +17,23 @@ class ConcurrencyDeferCommand extends Command
      */
     public function handle()
     {
-        //
+        Concurrency::defer([
+            function () {
+                sleep(2);
+                Log::info('Deferred 1');
+            },
+            function () {
+                sleep(2);
+                Log::info('Deferred 2');
+            },
+            function () {
+                sleep(2);
+                Log::info('Deferred 3');
+            },
+        ]);
+
+        $start = $_SERVER['REQUEST_TIME_FLOAT']; // або REQUEST_TIME
+        $now = microtime(true);
+        $this->info('Timeout: ' . $now - $start);
     }
 }
